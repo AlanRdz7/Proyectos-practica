2a3b4af7445888cef8334065a16fde56f906d008
AlanRDZ7 <alan.rodriguezast@uanl.edu.mx>
2025-12-17 23:17:20 -0600

    f

--- Proyectos-practica/Practica-integracionAPI.py
+++ Proyectos-practica/Practica-integracionAPI.py
@@ -30,7 +30,7 @@ def main():
         usuarios = obtener_usuarios()
         df = procesamiento_datos(usuarios)
         df.to_excel("usuarios.xlsx", index=False)
-        print("Archivo usuarios.xlsx generado correctamente")
         
         if __name__ == "__main__":
             main()
+            print("Archivo usuarios.xlsx generado correctamente")
